15cd315aaebe3c4a3b4e07c68d6304b4d2c8df48
difagume <difagume@gmail.com>
2023-03-11 19:43:44 -0500

    inicio codigo

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
+
+const PlaceholderImage = require("./assets/images/background-image.png");
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<View style={styles.imageContainer}>
+				<Image source={PlaceholderImage} style={styles.image} />
+			</View>
+			<StatusBar style="auto" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#25292e",
+		alignItems: "center",
+	},
+	imageContainer: {
+		flex: 1,
+		paddingTop: 58,
+	},
+	image: {
+		width: 320,
+		height: 440,
+		borderRadius: 18,
+	},
 });
